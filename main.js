46aef1558b072b6a7470b44ea2f0dfca52aec800
Langel <knoxburry@gmail.com>
2024-01-31 22:06:49 -0500

    stuff starting to work

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 
 
-browser.browserAction.onClicked.addListener((tab) => { 
+//browser.browserAction.onClicked.addListener((tab) => { 
 
-	console.log('triggered');
+console.log('triggered');
 
-	var arr = document.getElementsByClassName("CodeMirror-code");
+var arr = document.getElementsByClassName("CodeMirror-code");
+var file = arr[0];
 
-	var out = '';
+var code_out = '';
+var meta_out = '';
 
-	for (let i = 0; i < arr.length; i++) {
-		console.log(arr);
-		var kids = arr[i].children;
-		console.log(kids);
-		for (let j = 0; j < kids.length; j++) {
-			console.log(j);
-			out += kids[j].innerText;	
+for (let i = 0; i < 1; i++) {
+	console.log(arr);
+	var kids = arr[i].children;
+	console.log(kids);
+	for (let j = 0; j < kids.length; j++) {
+//		console.log(j);
+		var kidskid = kids[j].children;
+		for (let k = 0; k < kidskid.length; k++) {
+			code_out += kidskid[k].innerText.replace(/(\r?\n|\r)/gm, "");	
+			//code_out += kidskid[k].innerText;	
 		}
+		code_out += "\n";
 	}
+}
 
-	console.log(out);
+console.log(code_out);
 
-});
+//});
